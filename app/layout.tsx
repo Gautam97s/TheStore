649c3951df660dfde5a4afec112521a1516d4983
Gautam Sharma <gautam.mahi10@gmail.com>
2025-11-02 02:22:03 +0530

    tool changes 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,6 +74,28 @@ export default function RootLayout({
             `}
           </Script>
         )}
+        {/* ✅ Maze User Experience Analytics */}
+        {isProd && (
+          <Script id="maze-tracking" strategy="afterInteractive">
+            {`
+              (function (m, a, z, e) {
+                var s, t;
+                try {
+                  t = m.sessionStorage.getItem('maze-us');
+                } catch (err) {}
+                if (!t) {
+                  t = new Date().getTime();
+                  try { m.sessionStorage.setItem('maze-us', t); } catch (err) {}
+                }
+                s = a.createElement('script');
+                s.src = z + '?apiKey=' + e;
+                s.async = true;
+                a.getElementsByTagName('head')[0].appendChild(s);
+                m.mazeUniversalSnippetApiKey = e;
+              })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', 'f21d65ae-19f1-44ef-bf39-69bbff49f500');
+            `}
+          </Script>
+        )}
       </body>
     </html>
   );
